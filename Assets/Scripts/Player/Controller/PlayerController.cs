51fb4ceb3939382d4ba9330894c42d969a09993e
PeterGori <Peter.W.Gori@gmail.com>
2024-12-19 14:42:45 +0000

    Commented PlayerController Code

--- Assets/Scripts/Player/Controller/PlayerController.cs
+++ Assets/Scripts/Player/Controller/PlayerController.cs
@@ -42,11 +42,13 @@ public class PlayerController : MonoBehaviour
         ApplyMovementInput();
     }
 
+    // Checks all Overlap Colliders around the player and outputs booleans for all of them.
     private void CheckSurroundings()
     {
         Grounded = Physics2D.OverlapBox(GroundCheck.position, GroundCheckRadius, 0, TheGround);
     }
 
+    // Checks if the player is changing direction and flips the sprite if they are.
     private void CheckMovementDirection()
     {
         if (FacingRight && MovementInputDirection < 0)
@@ -61,29 +63,38 @@ public class PlayerController : MonoBehaviour
     
     private void CheckInput()
     {
+        // Gets the horizontal input from the player for use in movement and checking if the sprite should be flipped
         MovementInputDirection = Input.GetAxisRaw("Horizontal");
+        
+        // Checks if the player is grounded, if they are CoyoteTimeCounter is reset so the player can jump after walking off a ledge for a small window.
         if (Grounded)
         {
             CoyoteTimeCounter = CoyoteTime;
         }
+        // If the player is not grounded, the CoyoteTimeCounter is decremented by Time.deltaTime to limit the window the player can jump after walking off a ledge.
         else
         {
             CoyoteTimeCounter -= Time.deltaTime;
         }
+        
+        // If the player presses the space key, the JumpBufferCounter is reset. Jump Buffering is used to allow the player to jump if they press the space key before they hit the ground.
         if (Input.GetKeyDown(KeyCode.Space))
         {
             JumpBufferCounter = JumpBufferTime;
         }
+        // The JumpBufferCounter is decremented by Time.deltaTime to limit the window the player can press space in before hitting the ground, to reduce accidental jumps.
         else
         {
             JumpBufferCounter -= Time.deltaTime;
         }
-
+        
+        // To implement variable jump height, if the player releases space while they are still ascending at a rate greater than the MinJumpForce, the player's y velocity is set to the MinJumpForce.
         if (Input.GetKeyUp(KeyCode.Space) && PlayerRb.linearVelocity.y > MinJumpForce)
         {
             PlayerRb.linearVelocity = new Vector2(PlayerRb.linearVelocity.x, MinJumpForce);
         }
         
+        // Finally checks both the JumpBufferCounter and CoyoteTimeCounter to see if the player should be able to jump.
         if(JumpBufferCounter > 0 && CoyoteTimeCounter > 0f)
         {
             Jump();
@@ -91,17 +102,20 @@ public class PlayerController : MonoBehaviour
         }
     }
     
+    // Applies the movement input to the player's rigidbody using the MovementSpeed and MovementInputDirection.
     private void ApplyMovementInput()
     {
         PlayerRb.linearVelocity = new Vector2(MovementSpeed * MovementInputDirection, PlayerRb.linearVelocity.y);
     }
 
+    // Makes the player jump by setting the player's y velocity to the MaxJumpForce and playing the jump animation.
     private void Jump()
     {
         PlayerRb.linearVelocity = new Vector2(PlayerRb.linearVelocity.x, MaxJumpForce);
         PlayerAnimator.Jump();
     }
 
+    // Flips the player sprite when the player changes direction.
     private void Flip()
     {
         FacingRight = !FacingRight;
