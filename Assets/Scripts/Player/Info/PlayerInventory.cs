19b58859f066a8c6152beea586370c187c6ec6df
PeterGori <Peter.W.Gori@gmail.com>
2025-01-30 13:53:40 +0000

    Ice Fix + Inventory file created

---
+++ Assets/Scripts/Player/Info/PlayerInventory.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerInventory() : MonoBehaviour
+{
+    private PlayerInventory()
+    {
+        
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+}
